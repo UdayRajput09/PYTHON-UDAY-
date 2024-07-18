4be506f97596b428100176c571f9d7e758bb86d9
UdayRajput09 <144793828+UdayRajput09@users.noreply.github.com>
2024-07-18 09:20:42 +0530

    logic.py

--- PYTHON/ASSESSMENT/Python - Collections, Functions And Modules In Python/logic.py
+++ PYTHON/ASSESSMENT/Python - Collections, Functions And Modules In Python/logic.py
@@ -1,52 +1,46 @@
+# Global dictionary and counter
+game_data = {}
+counter = 1
 
-# creating class name Game
-class Game:
-    def __init__(self):
-        self.dic = {}
-        self.counter = 1
+def add():
+    global counter
+    question = input("Enter question: ")
+    option1 = input("Enter opt 1: ")
+    option2 = input("Enter opt 2: ")
+    right_answer = input("Enter Right Answer: ")
 
-    # creating add function
-    def add(self):
-        question = input("Enter question: ")
-        option1 = input("Enter opt 1: ")
-        option2 = input("Enter opt 2: ")
-        right_answer = input("Enter Right Answer: ")
+    game_data[counter] = {
+        'question': question,
+        'option1': option1,
+        'option2': option2,
+        'right_answer': right_answer
+    }
+    counter += 1
+    print("Question added successfully!")
 
-        self.dic[self.counter] = {
-            'question': question,
-            'option1': option1,
-            'option2': option2,
-            'right_answer': right_answer
-        }
-        self.counter += 1
-        print("Question added successfully!")
-
-    # creating read function
-    def read(self):
-        if not self.dic:
-            print("No questions available.")
-        else:
-            for i, j in self.dic.items():
-                print(f"{i}) {j['question']}")
-                print(f"A: {j['option1']}")
-                print(f"B: {j['option2']}")
-                answer = input("Enter answer: ")
-                if j['right_answer'] == answer:
-                    print("Correct Answer!!")
-                else:
-                    print(f"Wrong Answer. The correct answer is: {j['right_answer']}")
-
-
-    # creating delete function
-    def delete(self):
-        if not self.dic:
-            print("No question for delete!!")
-        else:
-            for i, j in self.dic.items():
-                print(f"{i} , {j['question']}")
-            delete = int(input("Enter question number to delete: "))
-            if delete in self.dic:
-                del self.dic[delete]
-                print(f"Deleted question {delete} successfully!!")
+def read():
+    if not game_data:
+        print("No questions available.")
+    else:
+        for i, j in game_data.items():
+            print(f"{i}) {j['question']}")
+            print(f"A: {j['option1']}")
+            print(f"B: {j['option2']}")
+            answer = input("Enter answer: ")
+            if j['right_answer'] == answer:
+                print("Correct Answer!!")
             else:
-                print("Invalid question number!!")
+                print(f"Wrong Answer. The correct answer is: {j['right_answer']}")
+
+def delete():
+    if not game_data:
+        print("No question for delete!!")
+    else:
+        for i, j in game_data.items():
+            print(f"{i} , {j['question']}")
+        delete = int(input("Enter question number to delete: "))
+        if delete in game_data:
+            del game_data[delete]
+            print(f"Deleted question {delete} successfully!!")
+        else:
+            print("Invalid question number!!")
